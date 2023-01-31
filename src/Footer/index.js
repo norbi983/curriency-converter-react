40aea05bfea8b5729e132e10df0c592adad899e1
Norbert Mszyca <norbi983@interia.pl>
2023-01-31 20:10:22 +0100

    change color and add CHF

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Footer = () => (
     <footer className="footer">
         <p className="footer__paragraph">
-            EUR 4.7199 | USD 4.7477 | GBP 5.4733 | 5.3451
+            EUR 4.7199 | USD 4.7477 | GBP 5.4733 | CHF 5.3451
         </p>
         <p className="footer__paragraph">
             kursy walut według średniego kursu NBP z dn. 28.10.2022
