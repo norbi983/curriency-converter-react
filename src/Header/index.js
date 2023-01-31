78df15150bd3a7a398b7903adf8a0e3f01e9525b
Norbert Mszyca <norbi983@interia.pl>
2023-01-31 20:59:25 +0100

    Updates

--- src/Header/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-const Header = () => (
-    <header className="header">
-        <h1 className="header__title">Kalkulator walut</h1>
-    </header>
-);
-
-export default Header;
