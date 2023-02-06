47079ebdd3b239cda2cbbd023366ea4967055383
Norbert Mszyca <norbi983@interia.pl>
2023-02-06 16:00:00 +0100

    updated result

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,7 +1,8 @@
 import "./style.css";
 
 const Result = ({ amount, rate, currency }) => {
-    const result =amount / rate;
+    const result = amount /
+        rate;
 
     return (
         <p className="form__result">{result.toFixed(2)} {currency}</p>
