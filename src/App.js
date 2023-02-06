33887eee980d85dd04291faf99dde3f4fbe9d7ac
Norbert Mszyca <norbi983@interia.pl>
2023-02-06 17:39:07 +0100

    added folder Clock

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Form from "./Form";
 import Container from "./Container";
 import Header from "./Header";
 import Footer from "./Footer";
+import Clock from "./Clock";
 import "./style.css";
+
 
 
 function App() {
     return (
         <Container>
             <Header />
+            <Clock />
             <Form />
             <Footer />
         </Container >
